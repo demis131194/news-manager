4ab3e592983f581d44fd2fe214a08692d167310c
Dzianis Katsuk <Dzianis_Katsuk@epam.com>
2020-01-29 16:56:29 +0300

    script (in progress).

--- repository-jdbc/src/main/resources/db/init-db.sql
+++ repository-jdbc/src/main/resources/db/init-db.sql
@@ -1,1 +1,138 @@
 CREATE DATABASE news;
+
+-- FIXME
+
+CREATE TABLE public.authors
+(
+    id integer NOT NULL DEFAULT nextval('authors_id_seq'::regclass),
+    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    surname character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT authors_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.authors
+    OWNER to postgres;
+
+
+CREATE TABLE public.news
+(
+    id integer NOT NULL DEFAULT nextval('news_id_seq'::regclass),
+    title character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    short_text character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    full_text character varying(2000) COLLATE pg_catalog."default" NOT NULL,
+    creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    modification_date timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT news_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.news
+    OWNER to postgres;
+
+
+CREATE TABLE public.news_authors
+(
+    news_id bigint NOT NULL,
+    author_id bigint NOT NULL,
+    CONSTRAINT news_authors_pkey PRIMARY KEY (news_id),
+    CONSTRAINT authors_id_foreign_key_constraint FOREIGN KEY (news_id)
+        REFERENCES public.authors (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT news_id_foreign_key_constraint FOREIGN KEY (news_id)
+        REFERENCES public.news (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.news_authors
+    OWNER to postgres;
+
+
+
+CREATE TABLE public.tags
+(
+    id integer NOT NULL DEFAULT nextval('tags_id_seq'::regclass),
+    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT tags_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.tags
+    OWNER to postgres;
+
+
+
+CREATE TABLE public.news_tags
+(
+    news_id bigint NOT NULL,
+    tag_id bigint NOT NULL,
+    CONSTRAINT news_tags_pkey PRIMARY KEY (news_id, tag_id),
+    CONSTRAINT news_id_foreign_key_constraint FOREIGN KEY (news_id)
+        REFERENCES public.news (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT tag_id_foreign_key_constraint FOREIGN KEY (tag_id)
+        REFERENCES public.tags (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.news_tags
+    OWNER to postgres;
+
+
+CREATE TABLE public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    surname character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    login character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
+
+
+
+CREATE TABLE public.roles
+(
+    user_id bigint NOT NULL,
+    role_name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT roles_pkey PRIMARY KEY (user_id),
+    CONSTRAINT user_id_foreign_key_constraint FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.roles
+    OWNER to postgres;
