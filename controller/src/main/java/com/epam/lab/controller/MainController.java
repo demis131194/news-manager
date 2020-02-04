24e24a460b0d421e4d68fd823992a7eba654ac07
Dzianis Katsuk <Dzianis_Katsuk@epam.com>
2020-02-04 17:45:00 +0300

    Add main controller.

---
+++ controller/src/main/java/com/epam/lab/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.epam.lab.controller;
+
+import com.epam.lab.model.Tag;
+import com.epam.lab.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/news-manager")
+public class MainController {
+
+    @Autowired
+    private TagService tagService;
+
+    @RequestMapping(value = "/news/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody Tag getTagById(@PathVariable("id") Long id) {
+        Tag tag = tagService.findById(id);
+        return tag;
+    }
+
+}
