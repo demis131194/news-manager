339b064402c951c0bdc094006771435dfc4a8869
Dzianis Katsuk <dzianis_katsuk@epam.com>
2020-03-05 12:05:07 +0000

    Revert "Add query collection for postman."
    This reverts commit fe4024aaf97fea5f73edb1fec8fe29c408affc84

--- controller/src/main/java/com/epam/lab/configuration/WebAppInitializer.java
+++ controller/src/main/java/com/epam/lab/configuration/WebAppInitializer.java
@@ -1,25 +1,21 @@
 package com.epam.lab.configuration;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-public class WebAppInitializer implements WebApplicationInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
-        webContext.register(AppContextConfig.class);
-        webContext.refresh();
-        webContext.setServletContext(servletContext);
-        servletContext.addListener(new ContextLoaderListener(webContext));
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("news", new DispatcherServlet(webContext));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/*");
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppContextConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected  String[] getServletMappings() {
+        return new String[]{"/"};
     }
 }
